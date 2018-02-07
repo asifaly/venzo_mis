37dc05c7184e98860b2395d9e696d20ce2025b9e
asifaly <asifaly82@yahoo.com>
2018-02-07 21:37:54 +0530

    fix scope date filter for postgres

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,9 +3,15 @@ class Assignment < ApplicationRecord
   belongs_to :task
   belongs_to :activity
 
-  scope :today, -> { where("cast(strftime('%d', task_date) as int) = ?", Time.now.day) }
-  scope :yesterday, -> { where("cast(strftime('%d', task_date) as int) = ?", Time.now.day - 1) }
-  scope :thismonth, -> { where("cast(strftime('%m', task_date) as int) = ?", Time.now.month) }
-  scope :lastmonth, -> { where("cast(strftime('%m', task_date) as int) = ?", Time.now.month - 1) }
+  # scope :today, -> { where("cast(strftime('%d', task_date) as int) = ?", Time.now.day) }
+  # scope :yesterday, -> { where("cast(strftime('%d', task_date) as int) = ?", Time.now.day - 1) }
+  # scope :thismonth, -> { where("cast(strftime('%m', task_date) as int) = ?", Time.now.month) }
+  # scope :lastmonth, -> { where("cast(strftime('%m', task_date) as int) = ?", Time.now.month - 1) }
   scope :byuser, -> (userid){ where(user_id: userid) }
+
+  scope :today, -> { where("extract(day from task_date) = ?", Time.now.day) }
+  scope :yesterday, -> { where("extract(day from task_date) = ?", Time.now.day - 1) }
+  scope :thismonth, -> { where("extract(month from task_date) = ?", Time.now.month) }
+  scope :lastmonth, -> { where("extract(month from task_date) = ?", Time.now.month - 1) }
+
 end
