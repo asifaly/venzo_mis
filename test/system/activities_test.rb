045f378de959f2e1c09f4d8824d1e4dd6bfc2951
asifaly <asifaly82@yahoo.com>
2018-03-11 12:14:35 +0530

    enabled basic system tests

--- test/system/activities_test.rb
+++ test/system/activities_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class ActivitiesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit activities_url
-  #
-  #   assert_selector "h1", text: "Activity"
-  # end
+  test "visiting the index" do
+    visit activities_url
+
+    assert_selector "h1", text: "Activities"
+  end
 end
--- test/system/bankcontacts_test.rb
+++ test/system/bankcontacts_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class BankcontactsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit bankcontacts_url
-  #
-  #   assert_selector "h1", text: "Bankcontact"
-  # end
+  test "visiting the index" do
+    visit bankcontacts_url
+
+    assert_selector "h1", text: "Bankcontacts"
+  end
 end
